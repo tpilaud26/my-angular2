b7b47a3deee58f6f8e5bbad82f5b7ed7228e8708
thomas pilaud <thomaspilaudpro26@gmail.com>
2022-04-05 16:21:54 +0200

    Mis en place la navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'portfolio', component:PortfolioComponent},
+  {path:'contact', component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
