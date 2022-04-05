19791d2caecb5550be43d9f57769123bfb9cc65e
thomas pilaud <thomaspilaudpro26@gmail.com>
2022-04-05 13:38:01 +0200

    js_angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
